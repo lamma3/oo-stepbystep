28af6ca000f926f881cf24c5a5dfbab563b979ad
Marco Lam <lamma3@oocl.com>
2020-04-07 14:52:11 +0800

    add first case implementation

---
+++ src/test/java/com/oocl/PersonTest.java
@@ -0,0 +1,16 @@
+package com.oocl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void testIntroduce_shouldReturnNameAndAge() {
+        Person person = new Person();
+        person.setName("Tom");
+        person.setAge(21);
+
+        Assert.assertEquals("My name is Tom. I am 21 years old.", person.introduce());
+    }
+}
